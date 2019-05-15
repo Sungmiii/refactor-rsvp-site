0aff0f16e91d8b2d6685d90e607d6905b6b84504
Sungmiii <christyksmile@gmail.com>
2019-05-15 15:49:44 +1200

    backend api and client setup and connect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+    const [response, setResponse] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get('http://localhost:9000/testAPI')
+            setResponse(res.data)
+        })()
+    }, [])
     return (
-        <div>App</div>
+        <div>get data from backend: {response}</div>
     )
-}
+};
 
 export default App;
